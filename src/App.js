aec606c409715ffda967ac43a600fdf8a7921d59
sandipan898 <sandipan.das898@gmail.com>
2022-05-13 23:32:41 +0530

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import Header from './common/Header';
+import Footer from './common/Footer';
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
+      <main>
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="dashboard" element={ <DashboardPage /> } />
+      </Routes>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
